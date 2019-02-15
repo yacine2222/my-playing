d75edc41fa9682ef8805ffbc410df98f57dba8d0
yacine2222 <42879964+yacine2222@users.noreply.github.com>
2019-02-15 19:30:25 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,45 +1,24 @@
-﻿const Discord = require('discord.js');
-const client = new Discord.Client();
- console.log("Scrpit By Yaaaz");
-
 const adminprefix = "Y";
-const developers = ['446446238452416538'];
-
-
-client.on('ready', () => {
-    console.log(`Logged as ${client.user.tag}By : Yaaz`)
-})
-
+const devs = ['446446238452416538','446446238452416538'];
 client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-  if (message.content.startsWith(adminprefix + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**تم تغيير البلاينق الى** :white_check_mark:   ``${argresult}``  `)
-  } else 
-     if (message.content === (adminprefix + "leave")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith(adminprefix + 'wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**تم تغيير الواتشينج الى** :white_check_mark:   ``${argresult}`` `)
-  } else 
-  if (message.content.startsWith(adminprefix + 'ls')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**تَم تغيير الليسينينق الى** :white_check_mark:  ``${argresult}``  `)
-  } else
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/Randy");
-      message.channel.send(`**تم تغييرك حالتك بالتويتش الى ** :white_check_mark:  ``${argresult}``  `)
-  }
-  if (message.content.startsWith(adminprefix + 'sn')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`جاري تغيير الأسم ل**ـ ...** :white_check_mark: ``${argresult}``  `)
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'بلاي')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+} else 
+  if (message.content.startsWith(adminprefix + 'نيم')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
 } else
-if (message.content.startsWith(adminprefix + 'sa')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`جاري تغيير الأفتار... :white_check_mark: : `);
+  if (message.content.startsWith(adminprefix + 'افتار')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else     
+if (message.content.startsWith(adminprefix + 'ستريم')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");//wennnn
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
 }
 });
-
-client.login(process.env.BOT_TOKEN);
